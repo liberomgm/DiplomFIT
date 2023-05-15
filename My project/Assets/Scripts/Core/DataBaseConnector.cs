1086cbb81c14f277b615bc8ad84bdb1baee18e84
shaman1979 <upgamestudio@gmail.com>
2023-05-15 15:35:29 +0300

    Создание базы данных

---
+++ My project/Assets/Scripts/Core/DataBaseConnector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+namespace Core
+{
+    public class DataBaseConnector
+    {
+        private SqliteConnection sqliteConnection;
+
+        public bool Connection(string dataBaseFileName)
+        {
+            try
+            {
+                var connection = Path.Combine("URI=file:", dataBaseFileName);
+
+                sqliteConnection?.Close();
+                sqliteConnection = new SqliteConnection(connection);
+
+                sqliteConnection.Open();
+
+                if (sqliteConnection.State != ConnectionState.Open)
+                {
+                    sqliteConnection = null;
+                    throw new InvalidOperationException("Подключение сорвано. Поток занят");
+                }
+
+                CreateDataBase();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void CreateDataBase()
+        {
+            ExecuteNonQuery(
+                "CREATE TABLE IF NOT EXISTS [db_users] ( " +
+                "[id] INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "[firstName] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[lastName] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[fatherName] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[password] VARCHAR(255)  NOT NULL, " +
+                "[phoneNumber] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[birthday] VARCHAR(255)  NOT NULL);");
+            
+            ExecuteNonQuery(
+                "CREATE TABLE IF NOT EXISTS [db_workouts] ( " +
+                "[id] INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL , " +
+                "[u_id] INTEGER  NOT NULL REFERENCES db_users (id) ON DELETE CASCADE ON UPDATE CASCADE, " +
+                "[c_id] INTEGER  NOT NULL REFERENCES db_coachs (id) ON DELETE CASCADE ON UPDATE CASCADE, " +
+                "[workoutDate] DATETIME NOT NULL, " +
+                "[workoutTime] DATETIME NOT NULL, " +
+                "[cost] REAL NOT NULL);");
+            
+            ExecuteNonQuery(
+                "CREATE TABLE IF NOT EXISTS [db_coachs] ( " +
+                "[id] INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "[firstName] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[lastName] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[fatherName] VARCHAR(255)  UNIQUE NOT NULL, " +
+                "[sum] VARCHAR(255) NOT NULL, " +
+                "[sportId] INTEGER  NOT NULL REFERENCES db_sports (id) ON DELETE CASCADE ON UPDATE CASCADE);");
+            
+            ExecuteNonQuery(
+                "CREATE TABLE IF NOT EXISTS [db_sports] ( " +
+                "[firstName] VARCHAR(255)  UNIQUE NOT NULL);");
+        }
+
+        private int ExecuteNonQuery(string query)
+        {
+            var result = 0;
+            if (sqliteConnection is { State: ConnectionState.Open })
+            {
+                try
+                {
+                    using var sqliteCommand = sqliteConnection.CreateCommand();
+
+                    sqliteCommand.CommandText = query;
+                    result = sqliteCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError(ex);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ My project/Assets/Scripts/Core/DatabaseProvider.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class DatabaseProvider : MonoBehaviour
+    {
+        private DataBaseConnector dataBaseConnector;
+        private const string DataBaseFileName = "DiplomFIT.db";
+        
+        public void Connecting()
+        {
+            dataBaseConnector = new DataBaseConnector();
+            if (dataBaseConnector.Connection(DataBaseFileName))
+            {
+                Debug.Log($"Подключение к базе данных статистики завершена успешно.");
+            }
+            else
+            {
+                Debug.LogError($"Подключение к базе данных статистики не завершено.");
+            }
+        }
+    }
+}
