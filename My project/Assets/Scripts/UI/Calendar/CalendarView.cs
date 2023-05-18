1100914b5f2eb2006a5c8cc650459c1d8c541a39
shamsutdinov_ad <upgamestudio@gmail.com>
2023-05-18 16:01:10 +0300

    Генерация календаря

---
+++ My project/Assets/Scripts/UI/Calendar/CalendarView.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+namespace UI.Calendar
+{
+    public class CalendarView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text monthTitle;
+        [SerializeField] private RectTransform dayContent;
+
+        [SerializeField] private GameObject dayPrefabs;
+
+        [SerializeField] private List<GameObject> days = new List<GameObject>();
+
+        public void FillingOut(int monthNumber)
+        {
+            monthTitle.text = MonthName(monthNumber);
+            var daysCount = DateTime.DaysInMonth(DateTime.Now.Year, monthNumber);
+
+            for (int i = 0; i < daysCount; i++)
+            {
+                days.Add(Instantiate(dayPrefabs, dayContent));
+            }
+        }
+
+        private string MonthName(int monthNumber)
+        {
+            switch (monthNumber)
+            {
+                case 1:
+                    return "Январь";
+                case 2:
+                    return "Февраль";
+                case 3:
+                    return "Март";
+                case 4:
+                    return "Апрель";
+                case 5:
+                    return "Май";
+                case 6:
+                    return "Июнь";
+                case 7:
+                    return "Июль";
+                case 8:
+                    return "Август";
+                case 9:
+                    return "Сентябрь";
+                case 10:
+                    return "Октябрь";
+                case 11:
+                    return "Ноябрь";
+                case 12:
+                    return "Декабрь";
+            }
+
+            throw new InvalidDataException();
+        }
+
+        public void Clear()
+        {
+            foreach (var day in days)
+            {
+                Destroy(day);
+            }
+            
+            days.Clear();
+        }
+    }
+}
--- My project/Assets/Scripts/UI/Windows/ScheduleWindow.cs
+++ My project/Assets/Scripts/UI/Windows/ScheduleWindow.cs
@@ -1,15 +1,21 @@
-﻿namespace UI.Windows
+﻿using System;
+using UI.Calendar;
+using UnityEngine;
+
+namespace UI.Windows
 {
     public class ScheduleWindow : Window
     {
+        [SerializeField] private CalendarView calendarView;
+        
         protected override void OnShow()
         {
-            
+            calendarView.FillingOut(DateTime.Now.Month);
         }
 
         protected override void OnHide()
         {
-            
+            calendarView.Clear();
         }
     }
 }
